ab0b9f3971d7e5f69cd843cf10bf08dd117f0114
kyeza <kyezaarnold63@gmail.com>
2019-02-26 22:34:44 +0300

    Implemented User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -11,3 +12,20 @@ class UserRegistrationForm(UserCreationForm):
 	 	"""docstring for Meta"""
  		model = User
  		fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+	"""docstring for UserUpdateForm"""
+	email = forms.EmailField()
+
+	class Meta:
+	 	"""docstring for Meta"""
+ 		model = User
+ 		fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+	"""docstring for ProfileUpdateForm"""
+
+	class Meta:
+	 	"""docstring for Meta"""
+ 		model = Profile
+ 		fields = ['image'] 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
 # Create your views here.
@@ -18,4 +18,20 @@ def register(request):
 
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		user_update_form = UserUpdateForm(request.POST, instance=request.user)
+		profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+		if user_update_form.is_valid() and profile_update_form.is_valid():
+			user_update_form.save()
+			profile_update_form.save()
+			messages.success(request, 'Your Profile has been updated')
+			return redirect('profile')
+	else:
+		user_update_form = UserUpdateForm(instance=request.user)
+		profile_update_form = ProfileUpdateForm(instance=request.user)
+
+	context = {
+		'user_update_form': user_update_form,
+		'profile_update_form': profile_update_form
+	}
+	return render(request, 'users/profile.html', context)
